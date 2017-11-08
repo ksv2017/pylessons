b8b7469aef69b8979918a1d0b9c589ede011b6a8
PanPetrovych <koshelyev@gmail.com>
2017-11-08 00:21:49 +0000

    adding db stuff into lesson 8

--- urok8_mimesis_and_database.py
+++ urok8_mimesis_and_database.py
@@ -5,6 +5,7 @@
 """
 from mimesis import Personal, Datetime, Address
 from mimesis.builtins import USASpecProvider
+import sqlite3
 
 
 def generate_fake_persons(size):
@@ -18,18 +19,42 @@ def generate_fake_persons(size):
 
     fake_persons = []
     for _ in range(0, size):
-        fake_person = {}
-        fake_person['email'] = person.email()
-        fake_person['uname'] = person.username()
-        fake_person['fname'] = person.full_name()
-        fake_person['dob'] = datetime.date(1970, 2000)
-        fake_person['address'] = address.address() + " - " + address.state()
-        fake_person['ssn'] = usa_provider.ssn()
+        fake_person = (person.email(), person.username(), person.full_name(), datetime.date(1970, 2000), \
+                       address.address() + " - " + address.state(), usa_provider.ssn())
         fake_persons.append(fake_person)
 
     return fake_persons
 
 
+def db_stuff(list_of_fake_persons):
+    """ This function creates a connection to the SQLite database (in the current directory)
+        and executes some basic SQL statements (create table, insert, select and drop table)
+    """
+    con = sqlite3.connect('test_baza.db')
+    cursor = con.cursor()
+
+    sql_create = 'CREATE TABLE peoples (email text, uname text, fname text, dob text, address text, ssn text)'
+    cursor.execute(sql_create)
+    con.commit()
+
+    sql_insert = 'INSERT INTO peoples VALUES (?,?,?,?,?,?)'
+    cursor.execute(sql_insert, list_of_fake_persons[0])
+    cursor.executemany(sql_insert, list_of_fake_persons[1:])
+    con.commit()
+    print('Total number of rows inserted:', con.total_changes)
+
+    sql_select = 'SELECT * FROM peoples'
+    rows_cursor = cursor.execute(sql_select)
+    for row in rows_cursor:
+        print(row)
+
+    sql_drop = 'DROP TABLE peoples'
+    cursor.execute(sql_drop)
+    con.commit()
+    print('Total number of rows deleted:', con.total_changes)
+    con.close()
+
+
 if __name__ == "__main__":
-    for _ in generate_fake_persons(5):
-        print(_)
+    list_of_fake_persons = generate_fake_persons(5)
+    db_stuff(list_of_fake_persons)
